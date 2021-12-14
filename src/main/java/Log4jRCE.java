df091e700d6ea6ff29cbf7d45d017afa317b5a98
TudbuT <tudbut@tudbut.de>
2021-12-14 20:18:36 +0100

    Made permanent patcher work with older log4j versions, clean some code

--- src/main/java/Log4jRCE.java
+++ src/main/java/Log4jRCE.java
@@ -1,24 +1,26 @@
-import org.apache.logging.log4j.LogManager;
+import jdk.internal.org.objectweb.asm.ClassReader;
+import jdk.internal.org.objectweb.asm.ClassWriter;
+import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.internal.org.objectweb.asm.tree.*;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.lookup.Interpolator;
 import org.apache.logging.log4j.core.lookup.StrLookup;
 import org.apache.logging.log4j.core.selector.ContextSelector;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.lang.reflect.Field;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.util.Base64;
+import java.util.Optional;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Log4jRCE {
@@ -26,29 +28,31 @@ public class Log4jRCE {
         Class<?> c = null;
         try {
             try { // Try for versions of Log4j >= 2.10
-              c = Thread.currentThread().getContextClassLoader().loadClass("org.apache.logging.log4j.core.util.Constants");
-              Field field = c.getField("FORMAT_MESSAGES_PATTERN_DISABLE_LOOKUPS");
-              System.out.println("Setting " + field.getName() + " value to True");
-              setFinalStatic(field, Boolean.TRUE);
+                c = Thread.currentThread().getContextClassLoader().loadClass("org.apache.logging.log4j.core.util.Constants");
+                Field field = c.getField("FORMAT_MESSAGES_PATTERN_DISABLE_LOOKUPS");
+                System.out.println("Setting " + field.getName() + " value to True");
+                setAccess(field);
+                field.set(null, Boolean.TRUE);
             } catch (NoSuchFieldException e) { // Fall back to older versions. Try to make JNDI non instantiable
                 c = null;
                 System.err.println("No field FORMAT_MESSAGES_PATTERN_DISABLE_LOOKUPS - version <= 2.9.0");
                 System.err.println("Will attempt to modify the configuration directly");
             }
 
-            //reconfiguring log4j
+            // Reconfigure log4j
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            Class<?> configuratorClass = classLoader.loadClass("org.apache.logging.log4j.core.config.Configurator");
+            c = classLoader.loadClass("org.apache.logging.log4j.core.config.Configurator");
             try {
-                Method reconfigure = configuratorClass.getMethod("reconfigure");
+                Method reconfigure = c.getMethod("reconfigure");
                 reconfigure.invoke(null);
             } catch (Exception ex) {
-                Method getFactoryMethod = configuratorClass.getDeclaredMethod("getFactory");
+                // Log4j is older.
+                Method getFactoryMethod = c.getDeclaredMethod("getFactory");
                 getFactoryMethod.setAccessible(true);
                 Object factory = getFactoryMethod.invoke(null);
                 Class<?> log4jContextFactoryClass = classLoader.loadClass("org.apache.logging.log4j.core.impl.Log4jContextFactory");
                 Method getSelector = log4jContextFactoryClass.getMethod("getSelector");
-                Object contextSelector = getSelector.invoke(factory, null);
+                Object contextSelector = getSelector.invoke(factory, (Object[]) null);
                 ContextSelector ctxSelector = (ContextSelector) contextSelector;
                 for (LoggerContext ctx: ctxSelector.getLoggerContexts()) {
                     ctx.reconfigure();
@@ -57,8 +61,9 @@ public class Log4jRCE {
                     StrLookup resolver = config.getStrSubstitutor().getVariableResolver();
                     if (resolver instanceof Interpolator) {
                         System.err.println("Lookup is an Interpolator - attempting to remove JNDI");
-                        Field lookups = null;
+                        Field lookups;
                         try {
+                            //noinspection JavaReflectionMemberAccess
                             lookups = Interpolator.class.getDeclaredField("lookups");
                         } catch (NoSuchFieldException e) {
                             lookups = Interpolator.class.getDeclaredField("strLookupMap");
@@ -69,12 +74,12 @@ public class Log4jRCE {
                     }
                 }
             }
-        } catch (Exception e) {
+        } catch (Throwable e) {
             System.err.println("Exception " + e);
             e.printStackTrace();
         }
 
-        // modify the log4j jar to fix the vuln
+        // Modify the log4j jar to fix the vulnerability permanently
         if(c != null)
             fullyVaccinate(c.getProtectionDomain().getCodeSource().getLocation());
     }
@@ -90,53 +95,99 @@ public class Log4jRCE {
                 ZipEntry entry;
                 while ((entry = in.getNextEntry()) != null) {
                     out.putNextEntry(new ZipEntry(entry.getName()));
-                    if(entry.getName().equals("org/apache/logging/log4j/core/util/Constants.class")) {
-                        // base64 of the patched class (FORMAT_MESSAGES_PATTERN_DISABLE_LOOKUPS set to default false)
-                        // base64 is more compact than a byte array in source code.
-                        byte[] newClass = Base64.getDecoder().decode("" +
-                                                                     "yv66vgAAADQAZAEALG9yZy9hcGFjaGUvbG9nZ2luZy9sb2c0ai9jb3JlL3V0aWwvQ29uc3RhbnRz" +
-                                                                     "BwABAQAQamF2YS9sYW5nL09iamVjdAcAAwEADkNvbnN0YW50cy5qYXZhAQAXTE9HNEpfTE9HX0VW" +
-                                                                     "RU5UX0ZBQ1RPUlkBABJMamF2YS9sYW5nL1N0cmluZzsBABRMb2c0akxvZ0V2ZW50RmFjdG9yeQgA" +
-                                                                     "CAEAFkxPRzRKX0NPTlRFWFRfU0VMRUNUT1IBABRMb2c0akNvbnRleHRTZWxlY3RvcggACwEAGkxP" +
-                                                                     "RzRKX0RFRkFVTFRfU1RBVFVTX0xFVkVMAQAXTG9nNGpEZWZhdWx0U3RhdHVzTGV2ZWwIAA4BABFK" +
-                                                                     "TkRJX0NPTlRFWFRfTkFNRQEAIGphdmE6Y29tcC9lbnYvbG9nNGovY29udGV4dC1uYW1lCAARAQAR" +
-                                                                     "TUlMTElTX0lOX1NFQ09ORFMBAAFJAwAAA+gBAB1GT1JNQVRfTUVTU0FHRVNfSU5fQkFDS0dST1VO" +
-                                                                     "RAEAAVoBACdGT1JNQVRfTUVTU0FHRVNfUEFUVEVSTl9ESVNBQkxFX0xPT0tVUFMBAApJU19XRUJf" +
-                                                                     "QVBQAQATRU5BQkxFX1RIUkVBRExPQ0FMUwEAFkVOQUJMRV9ESVJFQ1RfRU5DT0RFUlMBAB1JTklU" +
-                                                                     "SUFMX1JFVVNBQkxFX01FU1NBR0VfU0laRQEAGU1BWF9SRVVTQUJMRV9NRVNTQUdFX1NJWkUBABhF" +
-                                                                     "TkNPREVSX0NIQVJfQlVGRkVSX1NJWkUBABhFTkNPREVSX0JZVEVfQlVGRkVSX1NJWkUBAARzaXpl" +
-                                                                     "AQAWKExqYXZhL2xhbmcvU3RyaW5nO0kpSQEACHByb3BlcnR5AQAMZGVmYXVsdFZhbHVlAQAsb3Jn" +
-                                                                     "L2FwYWNoZS9sb2dnaW5nL2xvZzRqL3V0aWwvUHJvcGVydGllc1V0aWwHACQBAA1nZXRQcm9wZXJ0" +
-                                                                     "aWVzAQAwKClMb3JnL2FwYWNoZS9sb2dnaW5nL2xvZzRqL3V0aWwvUHJvcGVydGllc1V0aWw7DAAm" +
-                                                                     "ACcKACUAKAEAEmdldEludGVnZXJQcm9wZXJ0eQwAKgAhCgAlACsBAAY8aW5pdD4BAAMoKVYMAC0A" +
-                                                                     "LgoABAAvAQAEdGhpcwEALkxvcmcvYXBhY2hlL2xvZ2dpbmcvbG9nNGovY29yZS91dGlsL0NvbnN0" +
-                                                                     "YW50czsBAAg8Y2xpbml0PgEAFmxvZzRqLmZvcm1hdC5tc2cuYXN5bmMIADQBABJnZXRCb29sZWFu" +
-                                                                     "UHJvcGVydHkBABYoTGphdmEvbGFuZy9TdHJpbmc7WilaDAA2ADcKACUAOAwAFgAXCQACADoBABls" +
-                                                                     "b2c0ajIuZm9ybWF0TXNnTm9Mb29rdXBzCAA8DAAYABcJAAIAPgEAJ29yZy9hcGFjaGUvbG9nZ2lu" +
-                                                                     "Zy9sb2c0ai91dGlsL0NvbnN0YW50cwcAQAwAGQAXCQBBAEIJAAIAQgwAGgAXCQBBAEUJAAIARQEA" +
-                                                                     "HWxvZzRqMi5lbmFibGUuZGlyZWN0LmVuY29kZXJzCABIDAAbABcJAAIASgEAHGxvZzRqLmluaXRp" +
-                                                                     "YWxSZXVzYWJsZU1zZ1NpemUIAEwMACAAIQoAAgBODAAcABQJAAIAUAEAGGxvZzRqLm1heFJldXNh" +
-                                                                     "YmxlTXNnU2l6ZQgAUgwAHQAUCQACAFQBABxsb2c0ai5lbmNvZGVyLmNoYXJCdWZmZXJTaXplCABW" +
-                                                                     "DAAeABQJAAIAWAEAHGxvZzRqLmVuY29kZXIuYnl0ZUJ1ZmZlclNpemUIAFoMAB8AFAkAAgBcAQAN" +
-                                                                     "Q29uc3RhbnRWYWx1ZQEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBABJMb2NhbFZhcmlhYmxlVGFi" +
-                                                                     "bGUBABBNZXRob2RQYXJhbWV0ZXJzAQAKU291cmNlRmlsZQAxAAIABAAAAA4AGQAGAAcAAQBeAAAA" +
-                                                                     "AgAJABkACgAHAAEAXgAAAAIADAAZAA0ABwABAF4AAAACAA8AGQAQAAcAAQBeAAAAAgASABkAEwAU" +
-                                                                     "AAEAXgAAAAIAFQAZABYAFwAAABkAGAAXAAAAGQAZABcAAAAZABoAFwAAABkAGwAXAAAAGQAcABQA" +
-                                                                     "AAAZAB0AFAAAABkAHgAUAAAAGQAfABQAAAADAAoAIAAhAAIAXwAAAD0AAwACAAAACbgAKSobtgAs" +
-                                                                     "rAAAAAIAYAAAAAYAAQAAAIwAYQAAABYAAgAAAAkAIgAHAAAAAAAJACMAFAABAGIAAAAJAgAiABAA" +
-                                                                     "IwAQAAIALQAuAAEAXwAAADMAAQABAAAABSq3ADCxAAAAAgBgAAAACgACAAAAkgAEAJMAYQAAAAwA" +
-                                                                     "AQAAAAUAMQAyAAAACAAzAC4AAQBfAAAAlQADAAAAAABduAApEjUDtgA5swA7uAApEj0EtgA5swA/" +
-                                                                     "sgBDswBEsgBGswBHuAApEkkEtgA5swBLEk0RAIC4AE+zAFESUxECBrgAT7MAVRJXEQgAuABPswBZ" +
-                                                                     "ElsRIAC4AE+zAF2xAAAAAQBgAAAAJgAJAAAANgAMAD8AGABHAB4AUQAkAF4AMABqADsAdQBGAH4A" +
-                                                                     "UQCIAAEAYwAAAAIABQ==");
-                        out.write(newClass);
-                    }
-                    else {
-                        byte[] buf = new byte[4096];
-                        int i;
-                        while ((i = in.read(buf)) > 0) {
-                            out.write(buf, 0, i);
-                        }
+                    switch (entry.getName()) {
+                        case "org/apache/logging/log4j/core/util/Constants.class":
+                            // base64 of the patched class (FORMAT_MESSAGES_PATTERN_DISABLE_LOOKUPS set to default false)
+                            // base64 is more compact than a byte array in source code.
+                            byte[] newClass = Base64.getDecoder().decode("" +
+                                    "yv66vgAAADQAZAEALG9yZy9hcGFjaGUvbG9nZ2luZy9sb2c0ai9jb3JlL3V0aWwvQ29uc3RhbnRz" +
+                                    "BwABAQAQamF2YS9sYW5nL09iamVjdAcAAwEADkNvbnN0YW50cy5qYXZhAQAXTE9HNEpfTE9HX0VW" +
+                                    "RU5UX0ZBQ1RPUlkBABJMamF2YS9sYW5nL1N0cmluZzsBABRMb2c0akxvZ0V2ZW50RmFjdG9yeQgA" +
+                                    "CAEAFkxPRzRKX0NPTlRFWFRfU0VMRUNUT1IBABRMb2c0akNvbnRleHRTZWxlY3RvcggACwEAGkxP" +
+                                    "RzRKX0RFRkFVTFRfU1RBVFVTX0xFVkVMAQAXTG9nNGpEZWZhdWx0U3RhdHVzTGV2ZWwIAA4BABFK" +
+                                    "TkRJX0NPTlRFWFRfTkFNRQEAIGphdmE6Y29tcC9lbnYvbG9nNGovY29udGV4dC1uYW1lCAARAQAR" +
+                                    "TUlMTElTX0lOX1NFQ09ORFMBAAFJAwAAA+gBAB1GT1JNQVRfTUVTU0FHRVNfSU5fQkFDS0dST1VO" +
+                                    "RAEAAVoBACdGT1JNQVRfTUVTU0FHRVNfUEFUVEVSTl9ESVNBQkxFX0xPT0tVUFMBAApJU19XRUJf" +
+                                    "QVBQAQATRU5BQkxFX1RIUkVBRExPQ0FMUwEAFkVOQUJMRV9ESVJFQ1RfRU5DT0RFUlMBAB1JTklU" +
+                                    "SUFMX1JFVVNBQkxFX01FU1NBR0VfU0laRQEAGU1BWF9SRVVTQUJMRV9NRVNTQUdFX1NJWkUBABhF" +
+                                    "TkNPREVSX0NIQVJfQlVGRkVSX1NJWkUBABhFTkNPREVSX0JZVEVfQlVGRkVSX1NJWkUBAARzaXpl" +
+                                    "AQAWKExqYXZhL2xhbmcvU3RyaW5nO0kpSQEACHByb3BlcnR5AQAMZGVmYXVsdFZhbHVlAQAsb3Jn" +
+                                    "L2FwYWNoZS9sb2dnaW5nL2xvZzRqL3V0aWwvUHJvcGVydGllc1V0aWwHACQBAA1nZXRQcm9wZXJ0" +
+                                    "aWVzAQAwKClMb3JnL2FwYWNoZS9sb2dnaW5nL2xvZzRqL3V0aWwvUHJvcGVydGllc1V0aWw7DAAm" +
+                                    "ACcKACUAKAEAEmdldEludGVnZXJQcm9wZXJ0eQwAKgAhCgAlACsBAAY8aW5pdD4BAAMoKVYMAC0A" +
+                                    "LgoABAAvAQAEdGhpcwEALkxvcmcvYXBhY2hlL2xvZ2dpbmcvbG9nNGovY29yZS91dGlsL0NvbnN0" +
+                                    "YW50czsBAAg8Y2xpbml0PgEAFmxvZzRqLmZvcm1hdC5tc2cuYXN5bmMIADQBABJnZXRCb29sZWFu" +
+                                    "UHJvcGVydHkBABYoTGphdmEvbGFuZy9TdHJpbmc7WilaDAA2ADcKACUAOAwAFgAXCQACADoBABls" +
+                                    "b2c0ajIuZm9ybWF0TXNnTm9Mb29rdXBzCAA8DAAYABcJAAIAPgEAJ29yZy9hcGFjaGUvbG9nZ2lu" +
+                                    "Zy9sb2c0ai91dGlsL0NvbnN0YW50cwcAQAwAGQAXCQBBAEIJAAIAQgwAGgAXCQBBAEUJAAIARQEA" +
+                                    "HWxvZzRqMi5lbmFibGUuZGlyZWN0LmVuY29kZXJzCABIDAAbABcJAAIASgEAHGxvZzRqLmluaXRp" +
+                                    "YWxSZXVzYWJsZU1zZ1NpemUIAEwMACAAIQoAAgBODAAcABQJAAIAUAEAGGxvZzRqLm1heFJldXNh" +
+                                    "YmxlTXNnU2l6ZQgAUgwAHQAUCQACAFQBABxsb2c0ai5lbmNvZGVyLmNoYXJCdWZmZXJTaXplCABW" +
+                                    "DAAeABQJAAIAWAEAHGxvZzRqLmVuY29kZXIuYnl0ZUJ1ZmZlclNpemUIAFoMAB8AFAkAAgBcAQAN" +
+                                    "Q29uc3RhbnRWYWx1ZQEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBABJMb2NhbFZhcmlhYmxlVGFi" +
+                                    "bGUBABBNZXRob2RQYXJhbWV0ZXJzAQAKU291cmNlRmlsZQAxAAIABAAAAA4AGQAGAAcAAQBeAAAA" +
+                                    "AgAJABkACgAHAAEAXgAAAAIADAAZAA0ABwABAF4AAAACAA8AGQAQAAcAAQBeAAAAAgASABkAEwAU" +
+                                    "AAEAXgAAAAIAFQAZABYAFwAAABkAGAAXAAAAGQAZABcAAAAZABoAFwAAABkAGwAXAAAAGQAcABQA" +
+                                    "AAAZAB0AFAAAABkAHgAUAAAAGQAfABQAAAADAAoAIAAhAAIAXwAAAD0AAwACAAAACbgAKSobtgAs" +
+                                    "rAAAAAIAYAAAAAYAAQAAAIwAYQAAABYAAgAAAAkAIgAHAAAAAAAJACMAFAABAGIAAAAJAgAiABAA" +
+                                    "IwAQAAIALQAuAAEAXwAAADMAAQABAAAABSq3ADCxAAAAAgBgAAAACgACAAAAkgAEAJMAYQAAAAwA" +
+                                    "AQAAAAUAMQAyAAAACAAzAC4AAQBfAAAAlQADAAAAAABduAApEjUDtgA5swA7uAApEj0EtgA5swA/" +
+                                    "sgBDswBEsgBGswBHuAApEkkEtgA5swBLEk0RAIC4AE+zAFESUxECBrgAT7MAVRJXEQgAuABPswBZ" +
+                                    "ElsRIAC4AE+zAF2xAAAAAQBgAAAAJgAJAAAANgAMAD8AGABHAB4AUQAkAF4AMABqADsAdQBGAH4A" +
+                                    "UQCIAAEAYwAAAAIABQ==");
+                            out.write(newClass);
+                            break;
+                        case "org/apache/logging/log4j/core/lookup/Interpolator":
+                            try {
+                                // Read class
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                byte[] buf = new byte[4096];
+                                int i;
+                                while ((i = in.read(buf)) > 0) {
+                                    baos.write(buf, 0, i);
+                                }
+                                ClassReader classReader = new ClassReader(baos.toByteArray());
+                                ClassNode node = new ClassNode();
+                                classReader.accept(node, 0);
+
+                                FieldNode fn;
+                                {
+                                    Optional<FieldNode> optionalFieldNode = node.fields.stream().filter(fieldNode -> fieldNode.desc.contains("Map")).findFirst();
+                                    if(optionalFieldNode.isPresent())
+                                        fn = optionalFieldNode.get();
+                                    else
+                                        throw new NoSuchFieldException(); // This will be caught.
+                                }
+
+                                // Transform class
+                                InsnList patch = new InsnList();
+                                patch.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                                patch.add(new FieldInsnNode(Opcodes.GETFIELD, "org/apache/logging/log4j/core/lookup/Interpolator", fn.name, fn.desc));
+                                patch.add(new LdcInsnNode("jndi"));
+                                patch.add(new MethodInsnNode(Opcodes.INVOKEINTERFACE, "java/util/Map", "remove", "(Ljava/lang/Object;)Ljava/lang/Object;", true));
+                                patch.add(new InsnNode(Opcodes.POP));
+                                for (MethodNode method : node.methods) {
+                                    if ((method.access & Opcodes.ACC_STATIC) == 0)
+                                        method.instructions.insertBefore(method.instructions.getFirst(), patch);
+                                }
+
+                                // Write class back into zip file
+                                ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+                                node.accept(classWriter);
+                                out.write(classWriter.toByteArray());
+                            } catch (Throwable throwable) {
+                                System.err.println("Couldn't fully vaccinate! Deleting JNDILookup to make program crash when the exploit is used!!");
+                            }
+                            break;
+                        case "org/apache/logging/log4j/core/lookup/JndiLookup.class":
+                            break;
+                        default:
+                            byte[] buf = new byte[4096];
+                            int i;
+                            while ((i = in.read(buf)) > 0) {
+                                out.write(buf, 0, i);
+                            }
+                            break;
                     }
                     out.closeEntry();
                     in.closeEntry();
@@ -152,11 +203,6 @@ public class Log4jRCE {
                 e.printStackTrace();
             }
         }
-    }
-
-    static void setFinalStatic(Field field, Object newValue) throws Exception {
-        setAccess(field);
-        field.set(null, newValue);
     }
 
     private static void setAccess(Field field) throws NoSuchFieldException, IllegalAccessException {
