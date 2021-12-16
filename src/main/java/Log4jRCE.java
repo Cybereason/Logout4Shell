fa17391fb4a03fb006798d2258f738416d76884d
Yonatan Striem-Amit <yonatan@cybereason.com>
2021-12-16 17:41:59 -0500

    Working on perm fix

--- src/main/java/Log4jRCE.java
+++ src/main/java/Log4jRCE.java
@@ -89,8 +89,12 @@ public class Log4jRCE {
         }
 
         // Modify the log4j jar to fix the vulnerability permanently
-        if(c != null)
-            fullyVaccinate(c.getProtectionDomain().getCodeSource().getLocation());
+        try {
+           Thread.currentThread().getContextClassLoader().loadClass("org.apache.logging.log4j.core.util.Constants");
+           fullyVaccinate(c.getProtectionDomain().getCodeSource().getLocation());
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class JndiLookup not found. Not applying permanent vaccine");
+        }
     }
 
     private static void fullyVaccinate(URL jarfile) {
@@ -105,155 +109,7 @@ public class Log4jRCE {
                 while ((entry = in.getNextEntry()) != null) {
                     out.putNextEntry(new ZipEntry(entry.getName()));
                     switch (entry.getName()) {
-                        case "org/apache/logging/log4j/core/util/Constants.class":
-                            // base64 of the patched class (FORMAT_MESSAGES_PATTERN_DISABLE_LOOKUPS set to default false)
-                            // base64 is more compact than a byte array in source code.
-                            byte[] newClass = Base64.getDecoder().decode("" +
-                                    "yv66vgAAADQAZAEALG9yZy9hcGFjaGUvbG9nZ2luZy9sb2c0ai9jb3JlL3V0aWwvQ29uc3RhbnRz" +
-                                    "BwABAQAQamF2YS9sYW5nL09iamVjdAcAAwEADkNvbnN0YW50cy5qYXZhAQAXTE9HNEpfTE9HX0VW" +
-                                    "RU5UX0ZBQ1RPUlkBABJMamF2YS9sYW5nL1N0cmluZzsBABRMb2c0akxvZ0V2ZW50RmFjdG9yeQgA" +
-                                    "CAEAFkxPRzRKX0NPTlRFWFRfU0VMRUNUT1IBABRMb2c0akNvbnRleHRTZWxlY3RvcggACwEAGkxP" +
-                                    "RzRKX0RFRkFVTFRfU1RBVFVTX0xFVkVMAQAXTG9nNGpEZWZhdWx0U3RhdHVzTGV2ZWwIAA4BABFK" +
-                                    "TkRJX0NPTlRFWFRfTkFNRQEAIGphdmE6Y29tcC9lbnYvbG9nNGovY29udGV4dC1uYW1lCAARAQAR" +
-                                    "TUlMTElTX0lOX1NFQ09ORFMBAAFJAwAAA+gBAB1GT1JNQVRfTUVTU0FHRVNfSU5fQkFDS0dST1VO" +
-                                    "RAEAAVoBACdGT1JNQVRfTUVTU0FHRVNfUEFUVEVSTl9ESVNBQkxFX0xPT0tVUFMBAApJU19XRUJf" +
-                                    "QVBQAQATRU5BQkxFX1RIUkVBRExPQ0FMUwEAFkVOQUJMRV9ESVJFQ1RfRU5DT0RFUlMBAB1JTklU" +
-                                    "SUFMX1JFVVNBQkxFX01FU1NBR0VfU0laRQEAGU1BWF9SRVVTQUJMRV9NRVNTQUdFX1NJWkUBABhF" +
-                                    "TkNPREVSX0NIQVJfQlVGRkVSX1NJWkUBABhFTkNPREVSX0JZVEVfQlVGRkVSX1NJWkUBAARzaXpl" +
-                                    "AQAWKExqYXZhL2xhbmcvU3RyaW5nO0kpSQEACHByb3BlcnR5AQAMZGVmYXVsdFZhbHVlAQAsb3Jn" +
-                                    "L2FwYWNoZS9sb2dnaW5nL2xvZzRqL3V0aWwvUHJvcGVydGllc1V0aWwHACQBAA1nZXRQcm9wZXJ0" +
-                                    "aWVzAQAwKClMb3JnL2FwYWNoZS9sb2dnaW5nL2xvZzRqL3V0aWwvUHJvcGVydGllc1V0aWw7DAAm" +
-                                    "ACcKACUAKAEAEmdldEludGVnZXJQcm9wZXJ0eQwAKgAhCgAlACsBAAY8aW5pdD4BAAMoKVYMAC0A" +
-                                    "LgoABAAvAQAEdGhpcwEALkxvcmcvYXBhY2hlL2xvZ2dpbmcvbG9nNGovY29yZS91dGlsL0NvbnN0" +
-                                    "YW50czsBAAg8Y2xpbml0PgEAFmxvZzRqLmZvcm1hdC5tc2cuYXN5bmMIADQBABJnZXRCb29sZWFu" +
-                                    "UHJvcGVydHkBABYoTGphdmEvbGFuZy9TdHJpbmc7WilaDAA2ADcKACUAOAwAFgAXCQACADoBABls" +
-                                    "b2c0ajIuZm9ybWF0TXNnTm9Mb29rdXBzCAA8DAAYABcJAAIAPgEAJ29yZy9hcGFjaGUvbG9nZ2lu" +
-                                    "Zy9sb2c0ai91dGlsL0NvbnN0YW50cwcAQAwAGQAXCQBBAEIJAAIAQgwAGgAXCQBBAEUJAAIARQEA" +
-                                    "HWxvZzRqMi5lbmFibGUuZGlyZWN0LmVuY29kZXJzCABIDAAbABcJAAIASgEAHGxvZzRqLmluaXRp" +
-                                    "YWxSZXVzYWJsZU1zZ1NpemUIAEwMACAAIQoAAgBODAAcABQJAAIAUAEAGGxvZzRqLm1heFJldXNh" +
-                                    "YmxlTXNnU2l6ZQgAUgwAHQAUCQACAFQBABxsb2c0ai5lbmNvZGVyLmNoYXJCdWZmZXJTaXplCABW" +
-                                    "DAAeABQJAAIAWAEAHGxvZzRqLmVuY29kZXIuYnl0ZUJ1ZmZlclNpemUIAFoMAB8AFAkAAgBcAQAN" +
-                                    "Q29uc3RhbnRWYWx1ZQEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBABJMb2NhbFZhcmlhYmxlVGFi" +
-                                    "bGUBABBNZXRob2RQYXJhbWV0ZXJzAQAKU291cmNlRmlsZQAxAAIABAAAAA4AGQAGAAcAAQBeAAAA" +
-                                    "AgAJABkACgAHAAEAXgAAAAIADAAZAA0ABwABAF4AAAACAA8AGQAQAAcAAQBeAAAAAgASABkAEwAU" +
-                                    "AAEAXgAAAAIAFQAZABYAFwAAABkAGAAXAAAAGQAZABcAAAAZABoAFwAAABkAGwAXAAAAGQAcABQA" +
-                                    "AAAZAB0AFAAAABkAHgAUAAAAGQAfABQAAAADAAoAIAAhAAIAXwAAAD0AAwACAAAACbgAKSobtgAs" +
-                                    "rAAAAAIAYAAAAAYAAQAAAIwAYQAAABYAAgAAAAkAIgAHAAAAAAAJACMAFAABAGIAAAAJAgAiABAA" +
-                                    "IwAQAAIALQAuAAEAXwAAADMAAQABAAAABSq3ADCxAAAAAgBgAAAACgACAAAAkgAEAJMAYQAAAAwA" +
-                                    "AQAAAAUAMQAyAAAACAAzAC4AAQBfAAAAlQADAAAAAABduAApEjUDtgA5swA7uAApEj0EtgA5swA/" +
-                                    "sgBDswBEsgBGswBHuAApEkkEtgA5swBLEk0RAIC4AE+zAFESUxECBrgAT7MAVRJXEQgAuABPswBZ" +
-                                    "ElsRIAC4AE+zAF2xAAAAAQBgAAAAJgAJAAAANgAMAD8AGABHAB4AUQAkAF4AMABqADsAdQBGAH4A" +
-                                    "UQCIAAEAYwAAAAIABQ==");
-                            out.write(newClass);
-                            break;
-                        /* Commented out because of compatibility issues - JNDI will be deleted instead
-                           (which will help in almost the same way).
-                        --- BEGIN COMMENTED OUT CODE ---
-                        case "org/apache/logging/log4j/core/lookup/Interpolator":
-                            try {
-                                // Read class
-                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                                byte[] buf = new byte[4096];
-                                int i;
-                                while ((i = in.read(buf)) > 0) {
-                                    baos.write(buf, 0, i);
-                                }
-                                ClassReader classReader = new ClassReader(baos.toByteArray());
-                                ClassNode node = new ClassNode();
-                                classReader.accept(node, 0);
-
-                                FieldNode fn;
-                                {
-                                    Optional<FieldNode> optionalFieldNode = node.fields.stream().filter(fieldNode -> fieldNode.desc.contains("Map")).findFirst();
-                                    if(optionalFieldNode.isPresent())
-                                        fn = optionalFieldNode.get();
-                                    else
-                                        throw new NoSuchFieldException(); // This will be caught.
-                                }
-
-                                // Transform class
-                                InsnList patch = new InsnList();
-                                patch.add(new VarInsnNode(Opcodes.ALOAD, 0));
-                                patch.add(new FieldInsnNode(Opcodes.GETFIELD, "org/apache/logging/log4j/core/lookup/Interpolator", fn.name, fn.desc));
-                                patch.add(new LdcInsnNode("jndi"));
-                                patch.add(new MethodInsnNode(Opcodes.INVOKEINTERFACE, "java/util/Map", "remove", "(Ljava/lang/Object;)Ljava/lang/Object;", true));
-                                patch.add(new InsnNode(Opcodes.POP));
-                                for (MethodNode method : node.methods) {
-                                    if ((method.access & Opcodes.ACC_STATIC) == 0)
-                                        method.instructions.insertBefore(method.instructions.getFirst(), patch);
-                                }
-
-                                // Write class back into zip file
-                                ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-                                node.accept(classWriter);
-                                out.write(classWriter.toByteArray());
-                            } catch (Throwable throwable) {
-                                System.err.println("Couldn't fully vaccinate! Deleting JNDILookup to make program crash when the exploit is used!!");
-                            }
-                            break;
-                        --- END COMMENTED OUT CODE ---
-                         */
                         case "org/apache/logging/log4j/core/lookup/JndiLookup.class":
-                            break;
-                        case "META-INF/org/apache/logging/log4j/core/config/plugins/Log4j2Plugins.dat":
-                            DataOutputStream os = new DataOutputStream(out);
-                            DataInputStream is = new DataInputStream(new BufferedInputStream(in));
-
-                            // The old count
-                            int count = is.readInt();
-
-                            // Names of the visited categories
-                            String[] categoryNames = new String[count];
-                            // Counts in categories
-                            int[] ecounts = new int[count];
-                            // Bytes of the plugin entries, [category count][entries in category][bytes]
-                            byte[][][] bytes = new byte[count][][];
-                            for (int i = 0; i < count; i++) {
-                                // Category name is read and stores
-                                categoryNames[i] = is.readUTF();
-                                // Amount of entries in category is read and stored, ecounts[i] will be modified gradually.
-                                int ecount = ecounts[i] = is.readInt();
-                                // Initialize bytearray for this category
-                                bytes[i] = new byte[ecount][];
-
-                                // Loop through each plugin
-                                for (int j = 0; j < ecount; j++) {
-                                    // Create streams for this plugin
-                                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                                    DataOutputStream dos = new DataOutputStream(baos);
-                                    // Read the name
-                                    String name = is.readUTF();
-                                    // Copy plugin data to plugin byte array
-                                    dos.writeUTF(name);
-                                    dos.writeUTF(is.readUTF());
-                                    dos.writeUTF(is.readUTF());
-                                    dos.writeBoolean(is.readBoolean());
-                                    dos.writeBoolean(is.readBoolean());
-                                    // Store plugin byte array unless it is JNDI
-                                    if(name.equals("jndi")) {
-                                        ecounts[i]--;
-                                    }
-                                    else
-                                        bytes[i][j - (ecount - ecounts[i])] = baos.toByteArray();
-                                }
-                            }
-
-                            // Write back the data we just read
-                            os.writeInt(count);
-                            for (int i = 0; i < count; i++) {
-                                // Write category name
-                                os.writeUTF(categoryNames[i]);
-                                // Amount of plugins in category
-                                int ecount = ecounts[i];
-                                // Write number of plugins in category
-                                os.writeInt(ecount);
-
-                                // Loop through each plugin and write its bytes
-                                for (int j = 0; j < ecount; j++) {
-                                    os.write(bytes[i][j]);
-                                }
-                            }
-
                             break;
                         default:
                             byte[] buf = new byte[4096];
@@ -268,6 +124,8 @@ public class Log4jRCE {
                 }
                 in.close();
                 out.close();
+
+                System.out.println("Renaming " + fixedjar.getName() + " to " + jar.getName());
                 if (!jar.delete() || !fixedjar.renameTo(jar)) {
                     System.err.println("Couldn't patch jar.");
                 }
